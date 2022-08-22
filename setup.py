6e4076ec231ab3f69cfc140b2698e0bd36db203e
lacava <williamlacava@gmail.com>
2022-08-22 15:59:00 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='PMCBoost',
+      version='1.0',
+      description='Proportional Multicalibration Boosting',
+      author='William La Cava, Elle Lett',
+      author_email='williamlacava@gmail.com',
+      url='https://github.com/cavalab/pmcboost',
+      packages=['pmc']
+     )

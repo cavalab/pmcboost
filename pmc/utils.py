896cf345f1d4c50a6e70a0d0d6607630c2d9d086
lacava <williamlacava@gmail.com>
2023-01-24 17:39:36 -0500

    updates local

--- pmc/utils.py
+++ pmc/utils.py
@@ -34,4 +34,49 @@ def category_diff(cat1, cat2):
     else:
         return False
 
+def categorize(X, y, groups, grouping,
+               n_bins=10,
+               bins=None,
+               alpha=0.01,
+               gamma=0.01
+              ):
+    """Map data to an existing set of categories."""
+    assert isinstance(X, pd.DataFrame), "X should be a dataframe"
+
+    categories = None 
+
+    if bins is None:
+        bins = np.linspace(float(1.0/n_bins), 1.0, n_bins)
+        bins[0] = 0.0
+    else:
+        n_bins=len(bins)
+
+
+    df = X[groups].copy()
+    df.loc[:,'interval'], retbins = pd.cut(y, bins, 
+                                           include_lowest=True,
+                                           retbins=True
+                                          )
+    categories = {}
+    if grouping=='intersectional':
+        group_ids = df.groupby(groups).groups
+    elif grouping=='marginal':
+        group_ids = df[groups].groupby(groups).groups
+        group_ids = {}
+        for g in groups:
+            grp = df.groupby(g).groups
+            for k,v in grp.items():
+                group_ids[(g,k)] = v
+
+    min_grp_size = gamma*len(X) 
+    min_cat_size = min_grp_size*alpha/n_bins
+    for group, i in group_ids.items():
+        # filter groups smaller than gamma*len(X)
+        if len(i) <= min_grp_size:
+            continue
+        for interval, j in df.loc[i].groupby('interval').groups.items():
+            # filter categories smaller than alpha*gamma*len(X)/n_bins
+            if len(j) > min_cat_size:
+                categories[group + (interval,)] = j
+    return categories
 
